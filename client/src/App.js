1cbfe53dc6644472ebbf327d6f1ef687e3b45afc
richardswei <wei.richard.s@gmail.com>
2021-02-20 21:56:07 -0800

    adding server connectivity adding cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
+import React, {useEffect, useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [response, setResponse] = useState("")
+
+  async function callApi() {
+    try {
+      const res = await fetch(`http://localhost:9000/testApi`);
+      const json = await res.json();
+      console.log('json', json)
+      return json;
+    } catch (err) {
+      console.error('err', err);
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    callApi().then((res) => {
+      setResponse(`Response is ${res["success"]}`)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {response}
         </p>
         <a
           className="App-link"
